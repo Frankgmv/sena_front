3eddd6fa15f560abda7d7823d3aacbbb95ae21f1
KevaCG <kevacastrllin@gmail.com>
2024-02-25 00:36:33 -0500

    Admin dashboard + 2 pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import "toastr/build/toastr.css"
-import { BrowserRouter } from 'react-router-dom'
-import { CredentialProvider } from './context/AuthContext.jsx'
-import { UserProvider } from './context/UserContext.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import "toastr/build/toastr.css";
+import { BrowserRouter } from 'react-router-dom';
+import { CredentialProvider } from './context/AuthContext.jsx';
+import { UserProvider } from './context/UserContext.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CredentialProvider>
@@ -15,6 +16,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </BrowserRouter>
       </UserProvider>
     </CredentialProvider>
-
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/admin/menuInteractivo/MenuInteractivo.jsx
@@ -0,0 +1,11 @@
+import './MenuInteractivo.css'
+
+const MenuInteractivo = () => {
+  return (
+    <div className='menuInteractivo-body'>
+      <h1>Menu interactivo</h1>
+    </div>
+  )
+}
+
+export default MenuInteractivo
