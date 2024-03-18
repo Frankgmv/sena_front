4ee0afbf097e1fafeee04ba264176a0530c77349
Frankgmv <giovanyvelasquez2004@gmail.com>
2024-03-18 10:59:27 -0500

    add preview port

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.PORT),
+    port: parseInt(process.env.PORT) || 5371,
   },
+  preview: {
+    port: parseInt(process.env.PORT),
+  }
 });
