829cac700063d7f203ef3738e77fe57573b02442
Frankgmv <giovanyvelasquez2004@gmail.com>
2024-03-18 11:06:39 -0500

    add preview port

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 dotenv.config();
+const PORT = parseInt(process.env.PORT)
+console.log(PORT)
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.PORT) || 5371,
+    port: PORT || 5371,
   },
   preview: {
-    port: parseInt(process.env.PORT),
+    port: PORT,
   }
 });
