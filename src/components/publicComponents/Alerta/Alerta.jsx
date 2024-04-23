ca20ffeaf51c9113939b80435c83c86b25333995
KevaCG <kevacastrllin@gmail.com>
2024-04-23 15:52:19 -0500

    Organizacion de alerta

--- src/components/publicComponents/Alerta/Alerta.jsx
+++ src/components/publicComponents/Alerta/Alerta.jsx
@@ -8,6 +8,9 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { useDataGeneralContext } from '../../../context/publicContexts/DataGeneralContext';
 import { MOSTRAR_ARCHIVO } from '../../../assets/includes/variables';
 import { useNavigate } from 'react-router-dom';
+
+import CloseIcon from '@mui/icons-material/Close';
+
 const AlertaAnuncios = () => {
     const navegate = useNavigate()
     const { noticias: data } = useDataGeneralContext();
@@ -33,24 +36,43 @@ const AlertaAnuncios = () => {
     return (
         <div>
             <Dialog open={open} onClose={handleClose}>
-                <div style={{ backgroundColor: 'var(--grey-2)' }}>
-                    <DialogTitle style={{ fontWeight: 'bolder', color: '#000', marginBlock: '2vh', borderBottom: '2px solid var(--black)', textAlign: 'center', fontSize: '2em', fontWeight: 'bolder' }}>Atención</DialogTitle>
+                <div style={{ backgroundColor: 'var(--grey-2)', height: 'auto' }}>
+                    <div className="icon">
+                        <CloseIcon style={{position: 'absolute', right:'10', top: '10', cursor: 'pointer'}} onClick={handleClose} />
+                    </div>
+                    <DialogTitle style={{ fontWeight: 'bolder', color: '#000', marginBlock: '2vh', borderBottom: '2px solid var(--black)', textAlign: 'center', fontSize: '2em', fontWeight: 'bolder' }}>
+                        Atención
+                    </DialogTitle>
                     <DialogContent>
                         {noticiaAlerta ? (
-                            <DialogContentText style={{ fontWeight: 'bolder', color: '#000', fontSize: '1.4em', marginBlock: '2vh' }}>{noticiaAlerta.titulo}</DialogContentText>
+                            <DialogContentText style={{ fontWeight: 'bolder', color: '#000', fontSize: '1.7em', marginBlock: '2vh', textAlign: 'center' }}>
+                                {noticiaAlerta.titulo}
+                            </DialogContentText>
                         ) : (
-                            <DialogContentText style={{ fontWeight: 'bolder', color: '#000', marginBlock: '2vh' }}>Loading data...</DialogContentText>
+                            <DialogContentText style={{ fontWeight: 'bolder', color: '#000', marginBlock: '2vh', textAlign: 'center' }}>
+                                Cargando Contenido...
+                            </DialogContentText>
                         )}
-                        <div style={{ fontWeight: '500', color: '#000', textAlign: 'center', borderRadius: '7px', padding: '5px', border: '2px solid var(--black)' }}>
+                        <div style={{ fontWeight: '500', color: '#000', textAlign: 'center', padding: '5px' }}>
                             {noticiaAlerta?.encabezado && (
-                                <DialogContentText style={{ fontWeight: '500', color: '#000', textAlign: 'center', padding: '5px', borderBottom: '1px solid var(--black)' }}>{noticiaAlerta.encabezado}</DialogContentText>
+                                <DialogContentText style={{ fontWeight: '500', color: '#000', textAlign: 'center', padding: '5px', fontSize: '1.5em' }}>
+                                    {noticiaAlerta.encabezado}
+                                </DialogContentText>
                             )}
                             {noticiaAlerta?.descripcion && (
-                                <DialogContentText style={{ fontWeight: '500', color: '#000', textAlign: 'center', marginTop: '20px', padding: '6px', borderRadius: '7px', textAlign: 'justify' }}>{noticiaAlerta.descripcion}</DialogContentText>
+                                <DialogContentText style={{
+                                    fontWeight: '500',
+                                    color: '#000',
+                                    textAlign: 'center', marginTop: '20px',
+                                    padding: '20px 10%', borderRadius: '7px',
+                                }}>
+                                    {noticiaAlerta.descripcion}
+                                </DialogContentText>
+
                             )}
                         </div>
                         {noticiaAlerta?.imgPath && (
-                            <div style={{ width: '100%', marginTop: '20px', height: '150px', display: 'grid', placeItems: 'center', overflow: 'auto' }}>
+                            <div style={{ width: '100%', marginTop: '20px', height: '30vh', display: 'grid', placeItems: 'center' }}>
                                 <img style={{ width: '70%' }} src={MOSTRAR_ARCHIVO(noticiaAlerta.imgPath)} alt={noticiaAlerta.title} title={noticiaAlerta.title} />
                             </div>
                         )}
@@ -59,13 +81,11 @@ const AlertaAnuncios = () => {
                         <Button type='button' variant="contained" onClick={() => navegate('/noticias')} fullWidth style={{ marginBlock: '2vh' }}>
                             Ver noticias
                         </Button>
-                        <Button variant="contained" fullWidth onClick={handleClose} style={{ marginBlock: '2vh' }}>
-                            Cerrar
-                        </Button>
                     </DialogActions>
                 </div>
             </Dialog>
         </div>
+
     );
 };
 
