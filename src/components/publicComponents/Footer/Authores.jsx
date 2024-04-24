e8879fa8cb1120d607c036fe639ef81622bf45d4
Frankgmv <giovanyvelasquez2004@gmail.com>
2024-04-24 14:38:02 -0500

    cambios footer

--- src/components/publicComponents/Footer/Authores.jsx
+++ src/components/publicComponents/Footer/Authores.jsx
@@ -2,11 +2,11 @@ import "./authores.css"
 const Authores = ({url}) => {
   return (
     <div className="author">
-        <a className="author-item" target="_blank" href={url}>&copy; Derechos de reservados</a>
-        <a className="author-item" target="_blank" href={url}>Conoce nuestros creadores</a>
-        <a className="author-item" target="_blank" href={url}>Proyecto SENA 2023 - 2024</a>
+        <sapn className="author-item">&copy; Derechos de reservados</sapn>
+        <a className="author-item pointer" target="_blank" href={url}>Conoce nuestros creadores</a>
+        <span className="author-item" >Proyecto SENA 2023 - 2024</span>
     </div>
   )
 }
-
+  
 export default Authores
