1e6825ce8afb897bac08039091bcd8838a3466c5
Frankgmv <giovanyvelasquez2004@gmail.com>
2024-10-17 21:26:01 -0500

    mejora del Slider para dispositivos

--- src/components/publicComponents/Slider/Slider.jsx
+++ src/components/publicComponents/Slider/Slider.jsx
@@ -1,27 +1,28 @@
 import f2 from '../../../assets/img/f2.jpg'
 import './slider.css'
-import {MOSTRAR_ARCHIVO } from '../../../assets/includes/variables'
+import { MOSTRAR_ARCHIVO } from '../../../assets/includes/variables'
 import { useDataGeneralContext } from '../../../context/publicContexts/DataGeneralContext'
 import { useEffect } from 'react'
 
 const Slider = () => {
-    const {slider: data, getSlider} = useDataGeneralContext()
+    const { slider: data, getSlider } = useDataGeneralContext()
 
-    useEffect(()=>{
+    useEffect(() => {
         getSlider()
     }, [])
-    
+
     return (
         <div className="body_slider">
-            <div className="fondo"></div>
+            <div className="filtro">
+                <div className="slider_texto">
+                    <h2 className='centenerio'>Centenario Pereira</h2>
+                </div>
+            </div>
             <div className="slider">
                 <ul>
                     {data.map((item) => (
                         <li key={item.id}>
-                            <img  src={MOSTRAR_ARCHIVO(item.imagenes.imgPath)} alt="Imagen" onError={(e) => e.target.src = f2} />
-                            <div className="slider_texto">
-                                <h2>Centenario Pereira</h2>
-                            </div>
+                            <img src={MOSTRAR_ARCHIVO(item.imagenes.imgPath)} alt="Imagen" onError={(e) => e.target.src = f2} />
                         </li>
                     ))}
                 </ul>
